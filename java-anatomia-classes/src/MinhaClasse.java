1709c21d2170a2538d618c0d501f36e8fad97e4b
Juliana <juliana@yahoo.com.br>
2022-10-30 00:08:58 -0300

    meu primeiro teste em java

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    //[] -> array e args é abreviação de argumentos ou parâmetros
+ public static void main (String [] args) {
+    System.out.print( "Olá turma, sejam bem viados" ); ///tudo que for entre parênteses é método e será executado mas pra que ele possa ser executado ele precisa de algum parâmetro conforme suas expectativas
+
+
+    }
+
+}
