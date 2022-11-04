ec13d03b865276562d6c7475c3a1fcf95b7b2258
Juliana <juliana@yahoo.com.br>
2022-11-04 01:45:13 -0300

    primeiro hello world e comentários

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,6 @@
 public class MinhaClasse {
-    //[] -> array e args é abreviação de argumentos ou parâmetros
- public static void main (String [] args) {
+    // para se determinar um array utiliza-se: [] já args é abreviação de argumentos ou parâmetros
+ public static void main (String [] args) { //void significa que o programa não vair retornar nada
     System.out.print( "Olá turma, sejam bem viados" ); ///tudo que for entre parênteses é método e será executado mas pra que ele possa ser executado ele precisa de algum parâmetro conforme suas expectativas
 
 
